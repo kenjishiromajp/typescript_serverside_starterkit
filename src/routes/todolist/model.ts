ac12eae98f53333486a80c90ddfb579eacfa5ba8
Giovanni Kenji Shiroma <kenjishiromajp@gmail.com>
2018-06-01 01:38:28 -0300

    adicionando toJSON -> transform para o todolist e todo

--- src/routes/todolist/model.ts
+++ src/routes/todolist/model.ts
@@ -5,7 +5,15 @@ export const todoSchema = new Mongoose.Schema({
     name: { type: String, required: true },
     done: { type: Boolean, default: false },
 },{
-    timestamps: true
+    timestamps: true,
+    toJSON: {
+        transform: (doc, ret) => {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.__v;
+            return ret;
+        }
+    }
 })
 
 export const todoListSchema = new Mongoose.Schema({
@@ -13,6 +21,14 @@ export const todoListSchema = new Mongoose.Schema({
     todos: [todoSchema]
 },{
     timestamps: true,
+    toJSON: {
+        transform: (doc, ret) => {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.__v;
+            return ret;
+        }
+    }
 });
 
 export const todoListModel = Mongoose.model<ITodoList>("TodoList", todoListSchema, "todoLists");
